ff6617e3f282a117b144d87297e5b28b84d7e2ee
jakubzimny <jakubzimny@outlook.com>
2019-03-24 19:27:52 +0100

    initial

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(8,Calculator.Calculator.Add(5, 3));
+        }
+        [TestMethod]
+        public void TestMultiply()
+        {
+            Assert.AreEqual(8, Calculator.Calculator.Multiply(4, 2));
+        }
+    }
+}
